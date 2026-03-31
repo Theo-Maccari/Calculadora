600c3cbfb69fa9c9d45285750c5382fb21c4c9b1
theo-maccari <theo_maccari@estudante.sesisenai.org.br>
2026-03-31 08:27:45 -0300

    add - Início

---
+++ calculo.js
@@ -0,0 +1,14 @@
+let num1 = 0;
+let num2 = 0;
+let soma = 0;
+
+num1 = Number(prompt("Digite o primeiro número"));
+num2 = Number(prompt("Digite o segundo número"));
+
+function adição(){}
+
+function subtração(){}
+
+function multiplicação(){}
+
+function divisão(){}
